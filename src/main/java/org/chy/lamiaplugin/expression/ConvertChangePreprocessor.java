0db75139acce9193641c8e6cd1cf93493578af5c
Grampus <704188931@qq.com>
2023-11-07 22:45:20 +0800

    marker update

--- src/main/java/org/chy/lamiaplugin/expression/ConvertChangePreprocessor.java
+++ src/main/java/org/chy/lamiaplugin/expression/ConvertChangePreprocessor.java
@@ -4,13 +4,12 @@ import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiManager;
-import com.intellij.psi.PsiTreeChangeListener;
+import com.intellij.psi.*;
 import com.intellij.psi.impl.PsiTreeChangeEventImpl;
 import com.intellij.psi.impl.PsiTreeChangePreprocessor;
 import com.intellij.psi.search.FilenameIndex;
 import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.searches.ReferencesSearch;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
@@ -23,6 +22,14 @@ public class ConvertChangePreprocessor implements PsiTreeChangePreprocessor {
         this.project = project;
 
         Module module = ModuleManager.getInstance(project).getModules()[0];
+
+
+
+        PsiClass aClass = JavaPsiFacade.getInstance(project)
+                .findClass("com.chy.lamia.expose.Lamia", GlobalSearchScope.allScope(project));
+
+        Collection<PsiReference> all = ReferencesSearch.search(aClass).findAll();
+
         Collection<VirtualFile> java = FilenameIndex.getAllFilesByExt(project, "java", GlobalSearchScope.moduleScope(module));
         VirtualFile virtualFile = java.stream().findFirst().get();
         PsiFile file = PsiManager.getInstance(project).findFile(virtualFile);
