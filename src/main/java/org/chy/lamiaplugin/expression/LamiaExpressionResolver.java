8f2ea889470be4c5ad5a8bb34ca5469251764436
Grampus <704188931@qq.com>
2023-12-15 23:56:11 +0800

    marker update

--- src/main/java/org/chy/lamiaplugin/expression/LamiaExpressionResolver.java
+++ src/main/java/org/chy/lamiaplugin/expression/LamiaExpressionResolver.java
@@ -2,15 +2,16 @@ package org.chy.lamiaplugin.expression;
 
 import com.chy.lamia.convert.core.entity.LamiaConvertInfo;
 import com.chy.lamia.convert.core.entity.LamiaExpression;
-import com.chy.lamia.convert.core.expression.parse.entity.MethodWrapper;
+import com.chy.lamia.convert.core.entity.TypeDefinition;
+import com.chy.lamia.convert.core.entity.VarDefinition;
+import com.chy.lamia.convert.core.expression.parse.ConfigParseContext;
+import com.chy.lamia.convert.core.expression.parse.builder.BuilderContext;
+import com.chy.lamia.convert.core.expression.parse.builder.BuilderHandler;
 import com.intellij.psi.*;
 import com.siyeh.ig.psiutils.MethodCallUtils;
-import com.siyeh.ig.psiutils.VariableAccessUtils;
-import com.sun.tools.javac.tree.JCTree;
 import org.chy.lamiaplugin.expression.entity.PsiMethodWrapper;
 
 import java.util.List;
-import java.util.Set;
 
 public class LamiaExpressionResolver {
 
@@ -18,39 +19,74 @@ public class LamiaExpressionResolver {
         LamiaConvertInfo result = new LamiaConvertInfo();
         PsiMethodCallExpression methodCall = (PsiMethodCallExpression) psiElement;
 
-
-        return null;
+        LamiaExpression lamiaExpression = parseMethod(methodCall, result);
+        result.setLamiaExpression(lamiaExpression);
+        return result;
 
     }
 
-    private LamiaExpression parseMethod(PsiMethodCallExpression methodCall) {
+    private LamiaExpression parseMethod(PsiMethodCallExpression methodCall, LamiaConvertInfo convertInfo) {
         String name = MethodCallUtils.getMethodName(methodCall);
-
-        PsiMethodWrapper psiMethodWrapper = new PsiMethodWrapper(name, methodCall);
-
-
+        PsiMethodWrapper psiMethodWrapper = new PsiMethodWrapper(methodCall);
 
         // 结束方法一共有 3个  convert / setArgs / build
         if ("mapping".equals(name)) {
             LamiaExpression result = new LamiaExpression();
-            List<String> argsNames = fetchArgsName(data);
+            initPsiMethodWrapper(psiMethodWrapper, convertInfo);
+            List<String> argsNames = psiMethodWrapper.useAllArgsToName();
             result.addSpreadArgs(argsNames, null);
             return result;
         }
         if ("setField".equals(name)) {
             LamiaExpression result = new LamiaExpression();
-            List<String> argsNames = fetchArgsName(data);
+            initPsiMethodWrapper(psiMethodWrapper, convertInfo);
+            List<String> argsNames = psiMethodWrapper.useAllArgsToName();
             result.addArgs(argsNames);
             return result;
         }
 
-        if ("build".equals(name)) {
-            LamiaExpression result = new LamiaExpression();
-            parseBuildConfig(result, data);
-            return result;
+        if ("builder".equals(name)) {
+            return parseBuildConfig(psiMethodWrapper, convertInfo);
         }
 
         return null;
+    }
+
+    private LamiaExpression parseBuildConfig(PsiMethodWrapper psiMethodWrapper, LamiaConvertInfo convertInfo) {
+        LamiaExpression result = new LamiaExpression();
+        // 创建配置解析器 准备开始解析
+        ConfigParseContext context = new ConfigParseContext();
+
+        PsiMethodWrapper methodCallWrapper = psiMethodWrapper;
+
+        while (true) {
+            String name = methodCallWrapper.getName();
+            String key = context.getScope(name);
+            BuilderHandler handler = BuilderContext.getHandler(key);
+            if (handler == null) {
+                throw new RuntimeException("[LamiaExpressionResolver] 无法找到 配置处理器 key: [" + key + "]");
+            }
+
+            // 执行handler策略
+            handler.config(result, methodCallWrapper, context);
+
+            // 获取链式调用的下一个方法
+            PsiMethodCallExpression psiMethodCallExpression = nextMethodCall(methodCallWrapper.getMethodCallExpression());
+            if (psiMethodCallExpression == null){
+                return result;
+            }
+            methodCallWrapper = new PsiMethodWrapper(psiMethodCallExpression);
+            initPsiMethodWrapper(methodCallWrapper, convertInfo);
+        }
+    }
+
+    private void initPsiMethodWrapper(PsiMethodWrapper psiMethodWrapper, LamiaConvertInfo convertInfo) {
+        psiMethodWrapper.initArgs(psiArgWrapper -> {
+            PsiVariable psiVariable = psiArgWrapper.getPsiVariable();
+            PsiType type = psiVariable.getType();
+            convertInfo.addVarArgs(new VarDefinition(psiVariable.getName(), new TypeDefinition(type.getCanonicalText())));
+        });
+
     }
 
 
--- src/main/java/org/chy/lamiaplugin/expression/entity/PsiMethodWrapper.java
+++ src/main/java/org/chy/lamiaplugin/expression/entity/PsiMethodWrapper.java
@@ -2,8 +2,8 @@ package org.chy.lamiaplugin.expression.entity;
 
 import com.chy.lamia.convert.core.expression.parse.entity.ArgWrapper;
 import com.chy.lamia.convert.core.expression.parse.entity.MethodWrapper;
-import com.intellij.psi.PsiMethodCallExpression;
-import com.intellij.psi.PsiVariable;
+import com.intellij.psi.*;
+import com.siyeh.ig.psiutils.MethodCallUtils;
 import com.siyeh.ig.psiutils.VariableAccessUtils;
 import org.chy.lamiaplugin.expression.components.StringExpression;
 
@@ -16,13 +16,18 @@ public class PsiMethodWrapper extends MethodWrapper {
 
     private final PsiMethodCallExpression methodCallExpression;
 
-    public PsiMethodWrapper(String name, PsiMethodCallExpression methodCallExpression) {
-        super(name);
+    public PsiMethodWrapper(PsiMethodCallExpression methodCallExpression) {
+        super(MethodCallUtils.getMethodName(methodCallExpression));
         this.methodCallExpression = methodCallExpression;
     }
 
     public void initArgs(Consumer<PsiArgWrapper> useFun) {
-        Set<PsiVariable> methodCallParam = VariableAccessUtils.collectUsedVariables(methodCallExpression);
+        PsiExpressionList paramExpression = getParamExpression(methodCallExpression);
+        if (paramExpression == null) {
+            return;
+        }
+
+        Set<PsiVariable> methodCallParam = VariableAccessUtils.collectUsedVariables(paramExpression);
         List<ArgWrapper> arg = methodCallParam.stream().map(psiVariable -> {
             PsiArgWrapper psiArgWrapper = new PsiArgWrapper();
             psiArgWrapper.setName(psiVariable.getName());
@@ -32,5 +37,18 @@ public class PsiMethodWrapper extends MethodWrapper {
             return psiArgWrapper;
         }).collect(Collectors.toList());
         setArgs(arg);
+    }
+
+    public PsiExpressionList getParamExpression(PsiElement psiElement) {
+        for (PsiElement child : psiElement.getChildren()) {
+            if (child instanceof PsiExpressionList result) {
+                return result;
+            }
+        }
+        return null;
+    }
+
+    public PsiMethodCallExpression getMethodCallExpression() {
+        return methodCallExpression;
     }
 }
