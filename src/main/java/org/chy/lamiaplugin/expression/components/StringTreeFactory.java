01657c081b95a69cdd9d9638d042efc3fdcbf040
hengyuan.cao <hengyuan.cao@nio.com>
2024-03-01 01:07:20 +0800

    泛型显示修复

--- src/main/java/org/chy/lamiaplugin/expression/components/StringTreeFactory.java
+++ src/main/java/org/chy/lamiaplugin/expression/components/StringTreeFactory.java
@@ -6,23 +6,25 @@ import com.chy.lamia.convert.core.components.entity.Statement;
 import org.chy.lamiaplugin.expression.components.statement.IfStatement;
 import org.chy.lamiaplugin.expression.components.statement.StringStatement;
 
-import java.util.List;
+import java.util.*;
 
 public class StringTreeFactory implements TreeFactory {
 
     @Override
     public Expression newClass(String classPath, List<Expression> newInstanceParam) {
         String param = param(newInstanceParam);
-        StringExpression result = new StringExpression("new " + classAbb(classPath) + "(" + param + ")");
-        result.addImportClassPath(classPath);
+        ClassPathResult classPathResult = classPathHandle(classPath);
+        StringExpression result = new StringExpression("new " + classPathResult.simpleClassPath + "(" + param + ")");
+        classPathResult.allClassPath.forEach(result::addImportClassPath);
         return result;
     }
 
     @Override
     public Statement createVar(String instantName, String classPath, Expression newClass) {
         String value = toString(newClass);
-        StringStatement stringStatement = new StringStatement(classAbb(classPath) + " " + instantName + " = " + value);
-        stringStatement.addImportClassPath(classPath);
+        ClassPathResult classPathResult = classPathHandle(classPath);
+        StringStatement stringStatement = new StringStatement(classPathResult.simpleClassPath + " " + instantName + " = " + value);
+        classPathResult.allClassPath.forEach(stringStatement::addImportClassPath);
         return stringStatement;
     }
 
@@ -65,8 +67,9 @@ public class StringTreeFactory implements TreeFactory {
     @Override
     public Expression typeCast(String classPath, Expression expression) {
         String instant = toString(expression);
-        StringExpression stringExpression = new StringExpression("(" + classAbb(classPath) + ")" + instant);
-        stringExpression.addImportClassPath(classPath);
+        ClassPathResult classPathResult = classPathHandle(classPath);
+        StringExpression stringExpression = new StringExpression("(" + classPathResult.simpleClassPath + ")" + instant);
+        classPathResult.allClassPath.forEach(stringExpression::addImportClassPath);
         return stringExpression;
     }
 
@@ -136,14 +139,104 @@ public class StringTreeFactory implements TreeFactory {
 
     /**
      * 对 classpath 进行简写 比如 com.chy.User 只显示 User
+     * 同时返回所有的全路径，包括参与泛型的
+     * 如: java.util.Map<java.lang.String, java.lang.Object> map 将会返回 Map<String,Object> map, 同时返回 ["java.lang.String", "java.util.Map" , "java.lang.Object"]
      *
      * @return
      */
-    private String classAbb(String classPath) {
-        int index = classPath.lastIndexOf('.');
-        if (index < 0) {
-            return classPath;
+    private ClassPathResult classPathHandle(String classPath) {
+
+        char[] charArray = classPath.toCharArray();
+
+        // 3个索引从左到右分别代表 start 、 end、 lastDot
+        int[] index = new int[]{-1, -1, -1};
+        Set<String> allPath = new HashSet<>();
+        Map<String, String> allName = new HashMap<>();
+        StringBuilder resultBuilder = new StringBuilder();
+        for (int i = 0; i < charArray.length; i++) {
+            char c = charArray[i];
+            if (c == ' ') {
+                // 其中一段结束
+                classPathItemHandle(charArray, index, resultBuilder, allPath, allName);
+                continue;
+            }
+
+            if (c == '<' || c == '>' || c == ',') {
+                // 前面的结束，并且这个符号要一起跟后面
+                classPathItemHandle(charArray, index, resultBuilder, allPath, allName);
+                resultBuilder.append(c);
+                if (c == ','){
+                    resultBuilder.append(' ');
+                }
+                continue;
+            }
+            // 其他正常的字符去移动下标
+            if (c == '.') {
+                index[2] = i;
+                continue;
+            }
+            int start = index[0];
+            index[0] = start == -1 ? i : start;
+            index[1] = i;
         }
-        return classPath.substring(index + 1);
+
+        classPathItemHandle(charArray, index, resultBuilder, allPath, allName);
+
+        return new ClassPathResult(resultBuilder.toString(), allPath);
+
     }
+
+    private void classPathItemHandle(char[] charArray, int[] index, StringBuilder resultBuilder,
+                                     Set<String> allPath, Map<String, String> allName) {
+        int start = index[0];
+        int end = index[1];
+
+        if (start == -1 || end == -1) {
+            return;
+        }
+        String classPath = new String(charArray, start, end - start + 1);
+        allPath.add(classPath);
+
+        int dot = index[2];
+        String name;
+        if (dot == -1) {
+            name = classPath;
+        } else {
+            name = new String(charArray, dot + 1, end - dot);
+        }
+
+        String existAlisClassPath = allName.get(name);
+        // 这个别名不存在可以使用
+        if (existAlisClassPath == null) {
+            allName.put(name, classPath);
+
+        } else {
+            // 已经存在了看是否是相同类型
+            name = existAlisClassPath.equals(classPath) ? name : classPath;
+        }
+        resultBuilder.append(name);
+
+        // 使用完了，全部置空
+        index[0] = -1;
+        index[1] = -1;
+        index[2] = -1;
+    }
+
+
+
+    static class ClassPathResult {
+        public static ClassPathResult empty = new ClassPathResult("", new HashSet<>());
+
+        String simpleClassPath;
+        Set<String> allClassPath;
+
+        public ClassPathResult(String simpleClassPath, Set<String> allClassPath) {
+            this.simpleClassPath = simpleClassPath;
+            this.allClassPath = allClassPath;
+        }
+
+        public ClassPathResult() {
+        }
+    }
+
 }
